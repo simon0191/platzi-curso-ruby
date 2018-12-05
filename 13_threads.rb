8497e9ace3011767355fb814a473c39f896dfe67
Simon Soriano <simonsp@uber.com>
2018-12-05 17:04:51 -0500

    threads

---
+++ 13_threads.rb
@@ -0,0 +1,29 @@
+# threads.rb
+
+def without_threads
+  puts "Without threads"
+  start = Time.now
+  3.times { |i| http_call(i) }
+  puts "Total time: #{Time.now - start} seconds"
+  puts "----------------------------------------"
+end
+
+def with_threads
+  puts "With threads"
+  start = Time.now
+  threads = 3.times.map { |i| Thread.new { http_call(i) } }
+  #threads.map(&:join)
+  puts "Total time: #{Time.now - start} seconds"
+  puts "----------------------------------------"
+end
+
+# Ejecución sin threads
+
+
+def http_call(n)
+  puts "Simulating HTTP call #{n}"
+  sleep(1)
+end
+
+without_threads
+with_threads
