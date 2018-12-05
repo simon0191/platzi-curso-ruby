39a156b1c79686a8fcb68e653f94bae3d98d40a2
Simon Soriano <simonsp@uber.com>
2018-12-05 17:40:40 -0500

    generate-random-food

--- snake/src/actions/actions.rb
+++ snake/src/actions/actions.rb
@@ -4,7 +4,8 @@ module Actions
     next_position = calc_next_position(state)
     # verificar que la siguiente cassilla sea valida
     if position_is_food?(state, next_position)
-      grow_snake_to(state, next_position)
+      state = grow_snake_to(state, next_position)
+      generate_food(state)
     elsif position_is_valid?(state, next_position)
       move_snake_to(state, next_position)
     else
@@ -22,6 +23,12 @@ module Actions
   end
 
   private
+
+  def self.generate_food(state)
+    new_food = Model::Food.new(rand(state.grid.rows), rand(state.grid.cols))
+    state.food = new_food
+    state
+  end
 
   def self.position_is_food?(state, next_position)
     state.food.row == next_position.row && state.food.col == next_position.col
--- snake/test/actions_test.rb
+++ snake/test/actions_test.rb
@@ -1,4 +1,5 @@
 require "minitest/autorun"
+require "minitest/mock"
 require_relative "../src/actions/actions"
 require_relative "../src/model/state"
 
@@ -83,5 +84,35 @@ class ActionsTest < Minitest::Test
           Model::Coord.new(1,1),
           Model::Coord.new(0,1)
       ])
+  end
+
+  def test_generate_food
+    initial_state = Model::State.new(
+      Model::Snake.new([
+          Model::Coord.new(1,1),
+          Model::Coord.new(0,1)
+      ]),
+      Model::Food.new(2, 1),
+      Model::Grid.new(8, 12),
+      Model::Direction::DOWN,
+      false
+    )
+
+    expected_state = Model::State.new(
+      Model::Snake.new([
+          Model::Coord.new(2, 1),
+          Model::Coord.new(1,1),
+          Model::Coord.new(0,1)
+      ]),
+      Model::Food.new(0, 0),
+      Model::Grid.new(8, 12),
+      Model::Direction::DOWN,
+      false
+    )
+    
+    Actions.stub(:rand, 0) do
+      actual_state = Actions::move_snake(initial_state)
+      assert_equal actual_state, expected_state
+    end
   end
 end
